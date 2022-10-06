4ac1c764d410c6b1c10b95c44be9aa2037a43013
yh9035926 <yh9035926@naver.com>
2022-10-07 07:38:51 +0900

    study

--- src/components/home/homeMain.js
+++ src/components/home/homeMain.js
@@ -1,22 +1,24 @@
 import styled from "styled-components";
+import Counter from "./post/counter";
 import Post from "./post/post";
 import PostForm from "./post/postForm";
 
 const HomeMain = () => {
-    return (
-        <StyledWrap>
-            <PostForm />
-            <Post />
-        </StyledWrap>
-    );
+  return (
+    <StyledWrap>
+      <PostForm />
+      <Post />
+      <Counter></Counter>
+    </StyledWrap>
+  );
 };
 export default HomeMain;
 
 const StyledWrap = styled.div`
-    box-sizing: border-box;
-    max-width: 50rem;
-    min-width: 18.75rem;
-    width: 80%;
-    height: 100%;
-    margin: 0 auto;
+  box-sizing: border-box;
+  max-width: 50rem;
+  min-width: 18.75rem;
+  width: 80%;
+  height: 100%;
+  margin: 0 auto;
 `;
---
+++ src/components/home/post/counter.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Counter = () => {
+  return (
+    <div>
+      <button>+</button> 0
+    </div>
+  );
+};
+
+export default Counter;
